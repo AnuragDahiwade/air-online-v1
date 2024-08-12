a5d9f71e7ceda5b01b82c1b345931e66a0bef7a3
AnuragDahiwade <anurag.dahiwade@gmail.com>
2024-08-12 18:21:54 +0530

    header and hero v1 done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ NavbarComponent],
+  imports: [ NavbarComponent, HeroSectionComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
